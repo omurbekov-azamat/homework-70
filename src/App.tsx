578c56dc3d96003e17ad03f1931da69ce4580f7e
omurbekov-azamat <avgusttin8@gmail.com>
2022-12-23 20:40:25 +0600

    created component NavBar.tsx;

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Navbar from "./components/NavBar/NavBar";
 
 function App() {
   return (
-    <div className="container">
-      hello world!
-    </div>
+    <>
+      <Navbar/>
+    </>
   );
 }
 
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Link, NavLink} from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="navbar navbar-expand-sm navbar-dark bg-light">
+      <div className="container" style={{width: '800px'}}>
+        <NavLink to="/" className="nav-link">
+          Contacts
+        </NavLink>
+        <Link to='/new-contact' className='btn btn-primary'>
+          Add new contact
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
